c81962865e8603e485f1f2110bd26af8bae56172
ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2022-05-30 03:18:17 +0300

    create a new tab if the tab not found

--- popup.js
+++ popup.js
@@ -1,17 +1,31 @@
+const BASE_URL = 'localhost:3000'
+
+const getCurrentTabData = (tabs) => {
+    const url = tabs[0].url
+    const arr = tabs[0].title.split(' ')
+    const author = arr[arr.length - 7]
+    const [repo] = arr[arr.length - 1].split('/').slice(-1)
+    const name = arr.slice(0, -8).join(' ')
+
+    return {url, author, repo, name}
+}
+
 chrome.tabs.query({}, function(tabs) {
     const foundTab = tabs.find(tab => {
         let domain = (new URL(tab.url));
-        return domain.origin.includes('localhost:3000')
+        return domain.origin.includes(BASE_URL)
     })
 
-    const origin = new URL(foundTab.url).origin
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-        const url = tabs[0].url
-        const arr = tabs[0].title.split(' ')
-        const author = arr[arr.length - 7]
-        const [repo] = arr[arr.length - 1].split('/').slice(-1)
-        const name = arr.slice(0, -8).join(' ')
-
-        chrome.tabs.update(foundTab.id, {highlighted: true, url: `${origin}?url=${url}&name=${name}&author=${author}&repo=${repo}`});
-    });
+    if (foundTab) {
+        const origin = new URL(foundTab.url).origin
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            const {url, author, name, repo} = getCurrentTabData(tabs)
+            chrome.tabs.update(foundTab.id, {highlighted: true, url: `${origin}?url=${url}&name=${name}&author=${author}&repo=${repo}`});
+        });
+    } else {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            const {url, author, name, repo} = getCurrentTabData(tabs)
+            chrome.tabs.create({url: `${BASE_URL}?url=${url}&name=${name}&author=${author}&repo=${repo}`});
+        });
+    }
 } );
